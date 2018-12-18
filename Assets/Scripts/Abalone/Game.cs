06b448c1a69706797c4ec297cfd876cd54cb7274
Gidong Kwon <lucidfext@gmail.com>
2018-12-19 01:08:18 +0900

    Implement temporary board layout

--- Assets/Scripts/Abalone/Game.cs
+++ Assets/Scripts/Abalone/Game.cs
@@ -10,6 +10,14 @@ namespace Abalone
     {
         [SerializeField] private Transform marbleContainer;
         [SerializeField] private Marble marblePrefab;
+        [SerializeField] private Transform boardContainer;
+        [SerializeField] private GameObject boardTopLeftPrefab;
+        [SerializeField] private GameObject boardLeftPrefab;
+        [SerializeField] private GameObject boardBottomLeftPrefab;
+        [SerializeField] private GameObject boardBottomRightPrefab;
+        [SerializeField] private GameObject boardRightPrefab;
+        [SerializeField] private GameObject boardCenterTopRightPrefab;
+
         private BoardData boardData;
         private GameContext context;
 
@@ -27,14 +35,15 @@ namespace Abalone
             var size = boardData.arraySize;
             var cutThreshold = boardData.side - 1;
             var positionOffset = new AxialCoord(-cutThreshold, -cutThreshold);
-            // for (var x = 0; x < size; x++)
-            // {
-            //     for (var z = 0; z < size; z++)
-            //     {
-            //         if (Mathf.Abs(x + positionOffset.x + z + positionOffset.z) > cutThreshold) continue;
-            //         CreateMarble(0, new AxialCoord(x, z), positionOffset);
-            //     }
-            // } 
+
+            for (var x = 0; x < size; x++)
+            {
+                for (var z = 0; z < size; z++)
+                {
+                    if (Mathf.Abs(x + positionOffset.x + z + positionOffset.z) > cutThreshold) continue;
+                    CreateBoardInnerPart(new AxialCoord(x, z), positionOffset);
+                }
+            }
         }
 
         private void CreateMarbles()
@@ -61,6 +70,13 @@ namespace Abalone
 
             var marble = marbleObject.GetComponent<Marble>();
             marble.SetColor(context.GetPlayerColor(playerIndex));
+        }
+
+        private void CreateBoardInnerPart(AxialCoord coord, AxialCoord positionOffset)
+        {
+            var partObject = Instantiate(boardBottomLeftPrefab, (coord + positionOffset).ToWorld(), Quaternion.identity, boardContainer);
+            partObject.name = coord.ToString();
+            partObject.transform.localPosition += new Vector3(0, Constants.boardOffsetY, 0);
         }
     }
 }
